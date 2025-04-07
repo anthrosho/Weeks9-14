3ad7532d0ac503baab36587ed0106102ad176c8b
LAPTOP-C2EBQN26\Shoheet <gurunsho@sheridancollege.ca>
2025-04-07 11:31:25 -0400

    JAMESSS, RAVANDU! KYLEEE  GUNTHER

--- Assets/Systems/Script/characterChosen.cs
+++ Assets/Systems/Script/characterChosen.cs
@@ -1,27 +1,82 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class characterChosen : MonoBehaviour
+public class CharacterSelectionManager : MonoBehaviour
 {
-    public Button jamesButton;
-    public Button ravanduButton;
-    public Button kyleButton;
+    [Header("UI Elements")]
+    public Button[] characterButtons;
+    public Button beginCombatButton;
+    public Text selectionPrompt;
+
+    [Header("Settings")]
+    public string combatSceneName = "CombatScene";
+
+    private string player1Selection;
+    private string player2Selection;
+    private bool isPlayer1Turn = true;
 
     void Start()
     {
-        // James button
-        jamesButton.onClick.AddListener(() => {
-            Debug.Log("James chosen button was clicked!");
-        });
+        InitializeSelectionSystem();
+        beginCombatButton.gameObject.SetActive(false);
+    }
 
-        // Ravandu button
-        ravanduButton.onClick.AddListener(() => {
-            Debug.Log("Ravandu chosen button was clicked!");
-        });
+    void InitializeSelectionSystem()
+    {
+        foreach (Button button in characterButtons)
+        {
+            button.onClick.AddListener(() => HandleCharacterSelection(button.name));
+        }
 
-        // Kyle button
-        kyleButton.onClick.AddListener(() => {
-            Debug.Log("Kyle chosen button was clicked!");
-        });
+        UpdateSelectionPrompt();
+        beginCombatButton.onClick.AddListener(StartCombat);
+    }
+
+    void HandleCharacterSelection(string characterName)
+    {
+        if (isPlayer1Turn)
+        {
+            player1Selection = characterName;
+            DisableCharacterButton(characterName);
+        }
+        else
+        {
+            player2Selection = characterName;
+            foreach (Button button in characterButtons)
+            {
+                button.interactable = false;
+            }
+            beginCombatButton.gameObject.SetActive(true);
+        }
+
+        isPlayer1Turn = !isPlayer1Turn;
+        UpdateSelectionPrompt();
+    }
+
+    void DisableCharacterButton(string characterName)
+    {
+        foreach (Button button in characterButtons)
+        {
+            if (button.name == characterName)
+            {
+                button.interactable = false;
+                break;
+            }
+        }
+    }
+
+    void UpdateSelectionPrompt()
+    {
+        selectionPrompt.text = isPlayer1Turn ?
+            "Player 1: Choose Your Character" :
+            "Player 2: Choose Your Character";
+    }
+
+    void StartCombat()
+    {
+        PlayerPrefs.SetString("Player1Character", player1Selection);
+        PlayerPrefs.SetString("Player2Character", player2Selection);
+        SceneManager.LoadScene(combatSceneName);
     }
 }
